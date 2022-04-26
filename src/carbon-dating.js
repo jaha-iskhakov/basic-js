2ae72bfdc58b0e6df2e5e4466b97b9061a8fabee
Jakhongir <77614628+jaha-iskhakov@users.noreply.github.com>
2022-04-26 09:37:56 +0500

    docs: update carbon datings

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,7 +7,6 @@ module.exports = function dateSample(sampleActivity) {
   if (  typeof sampleActivity !== 'string' || arguments.length == 0 || sampleActivity > 15 || sampleActivity <= 0 || isNaN (sampleActivity) ) {
     return false;
   }
-
   let k = 0.693 / HALF_LIFE_PERIOD;
   let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
   return t;
